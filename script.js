2268bbe2801ee0b33db8520307d2bbdf79908c58
Maya Chisem <chisem.maya@gmail.com>
2020-03-09 11:05:08 -0700

    updated script

--- script.js
+++ script.js
@@ -30,4 +30,37 @@ var secondsLeft = 75;
  
  start.addEventListener('click', setTime);
 
+//quiz question array//
+var questions = [
+    {
+        title: "What is JavaScript?",
+        choices: ["A scripting language used to create dynamic website content",
+        "A form of CSS used to hardcode website styling","A place for writers to drink coffee",
+        "A conditional oriented programming language"],
+        answer: "A scripting language used to create dynamic website content"
+    },
+    {
+        title: "Objects that let you communicate in JS",
+        choices: ["Document Object Modal","Document Object Model",
+        "Designed Object Model","Data Object Modal"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "________ control behavior in JS and determines if pieces of code can run.",
+        choices: ["Arrays","JSON.parse",,"Booleans","Conditionals"],
+        answer: "Conditionals"
+    },
+    {
+        title: "This function allows a short syntax for writing function expressions.",
+        choices: ["Expression","Super","Arrow","Declaration"],
+        answer: "Arrow"
+    },
+    {
+        title: "Function parameters are:",
+        choices: ["Names listed in the function definition","Values passed to, and received by the function",
+        "",""],
+        answer: "Names listed in the function definition"
+    }
+]
 
+var hasAnswered = false;
