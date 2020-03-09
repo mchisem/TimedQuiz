ac0d733c4ccb5238efc3784d124bae2c13e4a63a
Maya Chisem <chisem.maya@gmail.com>
2020-03-09 03:37:47 -0700

    update

---
+++ script.js
@@ -0,0 +1,33 @@
+//variables//
+var start = document.querySelector("#start-button");
+var highScore = document.getElementById('score');
+var timer = document.getElementById('timer');
+var navbar = document.querySelector("#nav");
+var intro = document.querySelector("#intro");
+
+var secondsLeft = 75;
+
+let score = 0;
+
+//timer coundown funtion//
+var secondsLeft = 75;
+
+ function setTime() {
+     navbar.classList.remove("hide");
+     intro.classList.add("hide");
+
+     var timerInterval = setInterval(function(){
+         secondsLeft --;
+         timer.textContent = 'Time: ' + secondsLeft;
+
+         if(secondsLeft === 0) {
+             clearInterval(timerInterval);
+             console.log("over!");
+         }
+
+     }, 1000); 
+ }
+ 
+ start.addEventListener('click', setTime);
+
+
